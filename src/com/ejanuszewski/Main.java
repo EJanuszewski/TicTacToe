8ef1386084b782ad35158f6bc7b81ac1d324c6de
Edward Januszewski <e.januszewski@gmail.com>
2015-01-25 23:23:14 +0000

    Initial board and first move

--- src/com/ejanuszewski/Main.java
+++ src/com/ejanuszewski/Main.java
@@ -5,15 +5,20 @@ import java.util.Scanner;
 
 public class Main {
 
-    private static int option = 0;
-    private static Player player1, player2;
-    public static String coords;
+    private static Player player1, player2, currentPlayer;
+    public static String[] coords = new String[2];
+    public static int coordX, coordY = 0;
+    public static String[][] board = {
+        {"-","-","-"},
+        {"-","-","-"},
+        {"-","-","-"}
+    };
 
 
     private static class Player {
 
         private String name;
-        private char side;
+        private String side;
 
         public Player(String name) {
             this.name = name;
@@ -23,11 +28,11 @@ public class Main {
             return name;
         }
 
-        public char getSide() {
+        public String getSide() {
             return side;
         }
 
-        public void setSide(char side) {
+        public void setSide(String side) {
             this.side = side;
         }
     }
@@ -38,33 +43,46 @@ public class Main {
         player2 = new Player("Player 2");
 
         Scanner input = new Scanner(System.in);
-        System.out.print("Welcome to Tic Tac Toe\nPlease pick O's or X's for player 1");
-        if(input.next().toUpperCase() == "O") {
-            player1.setSide('O');
-            player2.setSide('X');
+        System.out.print("Welcome to Tic Tac Toe\nPlease pick O's or X's for player 1: ");
+        if(input.next().toUpperCase() == "X") {
+            player1.setSide("X");
+            player2.setSide("O");
         } else {
-            player1.setSide('X');
-            player2.setSide('O');
+            player1.setSide("O");
+            player2.setSide("X");
         }
         //X goes first
-        if(player1.getSide() == 'X') {
-            System.out.print(player1.getName() + " goes first.\nPlease enter the X and Y coordinates seperated by a comma, e.g. 1,2");
+        if(player1.getSide() == "X") {
+            currentPlayer = player1;
+        } else {
+            currentPlayer = player2;
         }
-        coords = input.next();
+        System.out.println(currentPlayer.getName() + " goes first.\nPlease enter the X and Y coordinates separated by a comma, e.g. 1,2: ");
+        setMarker(currentPlayer, input.next());
+
+        drawBoard();
 
         System.out.println();
 
         //MainMenu();
     }
 
-    private static void drawBoard() {
-        System.out.println("___|___|___");
-        System.out.println("___|___|___");
-        System.out.println("   |   |   ");
+    public static void drawBoard() {
+        for(String[] row : board) {
+            System.out.println(row[0] + "|" + row[1] + "|" + row[2]);
+        }
+//        System.out.println("___|___|___");
+//        System.out.println("___|___|___");
+//        System.out.println("   |   |   ");
     }
 
     private static void setMarker(Player player, String position) {
-        //x = position.split(",");
+        coords = position.split(",");
+        coordX = Integer.parseInt(coords[0]) - 1;
+        coordY = Integer.parseInt(coords[1]) - 1;
+        if(board[coordY][coordX] == "-") {
+            board[coordY][coordX] = player.getSide();
+        }
     }
 
     /*private static int MainMenu() {
