66686012ea13dc64eb1bc828737e9c4b1b6d1395
Edward Januszewski <e.januszewski@gmail.com>
2015-01-27 14:23:39 +0000

    Swapping sides and horizontal winner calculation

--- src/com/ejanuszewski/Main.java
+++ src/com/ejanuszewski/Main.java
@@ -8,6 +8,7 @@ public class Main {
     private static Player player1, player2, currentPlayer;
     public static String[] coords = new String[2];
     public static int coordX, coordY = 0;
+    public static String input;
     public static String[][] board = {
         {"-","-","-"},
         {"-","-","-"},
@@ -42,9 +43,10 @@ public class Main {
         player1 = new Player("Player 1");
         player2 = new Player("Player 2");
 
-        Scanner input = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         System.out.print("Welcome to Tic Tac Toe\nPlease pick O's or X's for player 1: ");
-        if(input.next().toUpperCase() == "X") {
+        input = scanner.next().toUpperCase();
+        if(input.equals("X")) {
             player1.setSide("X");
             player2.setSide("O");
         } else {
@@ -52,19 +54,12 @@ public class Main {
             player2.setSide("X");
         }
         //X goes first
-        if(player1.getSide() == "X") {
+        if(player1.getSide().equals("X")) {
             currentPlayer = player1;
         } else {
             currentPlayer = player2;
         }
-        System.out.println(currentPlayer.getName() + " goes first.\nPlease enter the X and Y coordinates separated by a comma, e.g. 1,2: ");
-        setMarker(currentPlayer, input.next());
-
-        drawBoard();
-
-        System.out.println();
-
-        //MainMenu();
+        takeGo();
     }
 
     public static void drawBoard() {
@@ -76,13 +71,63 @@ public class Main {
 //        System.out.println("   |   |   ");
     }
 
-    private static void setMarker(Player player, String position) {
+    private static void takeGo() {
+        System.out.println(currentPlayer.getName() + "'s turn.");
+        System.out.print("Please enter the X and Y coordinates separated by a comma, e.g. 1,2: ");
+        Scanner scanner = new Scanner(System.in);
+        input = scanner.next().toUpperCase();
+
+        try {
+            if(setMarker(currentPlayer, input)) {
+                drawBoard();
+
+                //Check if that move the game
+                if(!hasWon(currentPlayer)) {
+                    if (currentPlayer == player1) {
+                        currentPlayer = player2;
+                    } else {
+                        currentPlayer = player1;
+                    }
+                    takeGo();
+                } else {
+                    System.out.printf("We have a winner!, congratulations %s", currentPlayer.getName());
+                }
+
+            } else {
+                System.out.println("That space is already taken, pick another!");
+            }
+        } catch(ArrayIndexOutOfBoundsException e) {
+            System.out.println("Please ensure you enter 3 or less for both coordinates");
+            takeGo();
+        }
+    }
+
+    private static boolean setMarker(Player player, String position) {
         coords = position.split(",");
         coordX = Integer.parseInt(coords[0]) - 1;
         coordY = Integer.parseInt(coords[1]) - 1;
-        if(board[coordY][coordX] == "-") {
+        if(board[coordY][coordX].equals("-")) {
             board[coordY][coordX] = player.getSide();
+            return true;
+        } else {
+            return false;
         }
+    }
+
+    private static boolean hasWon(Player player) {
+        String side = player.getSide();
+        int rowCount = 0;
+        for(String[] row : board) {
+            //Horizontal line
+            for(String cell : row) {
+                if(cell.equals(side))
+                    rowCount += 1;
+            }
+            if(rowCount == 3)
+                return true;//Winner
+        }
+
+        return false;
     }
 
     /*private static int MainMenu() {
